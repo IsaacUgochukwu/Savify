aa02b0df4e859feb187bccf08286a48ae0360a6a
Dev Isaac <isaaczik9@gmail.com>
2024-12-12 19:38:47 +0100

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from './Head.jsx'
+import './styles/style.css'
+import './styles/bootstrap.min.css'
+
+const App = () => {
+  <>
+   <Header />
+  </>
+}
+
+export default App
